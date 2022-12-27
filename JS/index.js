452b06293f34be415bdd0b44e1f38165d2d1aeb7
Văn Gia Quyền <giaquyenvan@gmail.com>
2022-12-27 12:17:05 +0700

    Update dark nav

--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,7 @@
 document.getElementById("switchButton").onclick = function () {
     document.getElementById("myBody").classList.toggle("dark");
     document.getElementById("lesson").classList.toggle("bg-light");
+    document.getElementById("nav").classList.toggle("bg-light");
+    document.getElementById("head").classList.toggle("dark2");
   };
   
