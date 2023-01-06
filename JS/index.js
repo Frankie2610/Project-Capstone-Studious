7138733fed9ed09af606a774e0cb923672afb2c2
Văn Gia Quyền <giaquyenvan@gmail.com>
2023-01-07 03:42:37 +0700

    done

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,20 @@
 
 document.getElementById("switchButton").onclick = function () {
-    document.getElementById("myBody").classList.toggle("dark");
-    document.getElementById("lesson").classList.toggle("bg-light");
-    document.getElementById("nav").classList.toggle("bg-light");
-    document.getElementById("head").classList.toggle("dark2");
-    document.getElementById("lesson").classList.toggle("dark3");
-  };
-  
+  document.getElementById("myBody").classList.toggle("dark");
+  document.getElementById("lesson").classList.toggle("bg-light2");
+  document.getElementById("head").classList.toggle("dark2");
+  document.getElementById("lesson").classList.toggle("dark3");
+};
+
+// document.getElementById("eraser").onclick = function () {
+//   document.getElementById("search").value =""
+//   document.getElementById("eraser").classList.add("d-none")
+// }
+// document.getElementById("search").onclick = function () {
+//   document.getElementById("eraser").classList.add("d-inline")
+// }
+
+// document.getElementById("buttonExpand").onclick = function () {
+//   document.getElementById("expand").classList.add("none")
+//   document.getElementById("close").classList.add("none")
+// }
